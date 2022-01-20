280d1d3a6fa666cac0c5b165c2728f460b8908d3
Ulisses Ruiz G <uruiz@philias.mx>
2022-01-20 11:13:08 -0600

    api basic connection an use formik

---
+++ images-finder/src/App.js
@@ -0,0 +1,46 @@
+import {Field, Form, Formik} from 'formik';
+import { useState } from 'react';
+import './css/header.css';
+import './css/content.css';
+import './css/article.css';
+
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const open = url => window.open(url);
+  return (
+    <div>
+      <header>
+        <Formik
+         initialValues={{ search: ''}}
+         onSubmit={async values => {
+            const response = await  fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,{
+              headers: {
+                'Authorization': 'Client-ID api-key-here',
+              }
+            });
+
+            const data = await  response.json();
+            setPhotos(data.results);
+         }}
+        >
+        <Form>
+          <Field name="search"/>
+        </Form>
+        </Formik>
+      </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo =>
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular}/>
+              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+            </article>
+
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
